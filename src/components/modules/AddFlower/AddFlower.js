c9f7bd3c09933825e32436c2cddd775af27eda69
Jakub Granos <jakubgranos@gmail.com>
2021-07-07 23:41:06 +0200

    update form

--- src/components/modules/AddFlower/AddFlower.js
+++ src/components/modules/AddFlower/AddFlower.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { AddFlowerForm, AddFlowerFormImage, AddFlowerInputsWrapper, AddFlowerInput, AddFlowerWrapper, AddFlowerHint } from './style';
+import { AddFlowerForm, AddFlowerFormImage, AddFlowerInputsWrapper, AddFlowerInputFile, AddFlowerWrapper, AddFlowerHint, AddFlowerInput, AddFlowerTextArea } from './style';
 import ImagesPlaceholder from '../../../assets/images/images-placeholder.png'
-
+import Pot from '../../../assets/images/pot.png'
+import { css } from "@emotion/css";
 const AddFlower = (props) => {
 	const [active, setActive] = useState('');
 	return (
@@ -10,7 +11,23 @@ const AddFlower = (props) => {
 			<AddFlowerForm>
 				<AddFlowerInputsWrapper>
 					<AddFlowerFormImage src={ImagesPlaceholder} alt="image placeholder" />
-					<AddFlowerInput type="file" />
+					<AddFlowerInputFile type="file" />
+				</AddFlowerInputsWrapper>
+				<AddFlowerInputsWrapper>
+					<AddFlowerInput type="text" placeholder="Nazwa kwiatka" />
+				</AddFlowerInputsWrapper>
+				<AddFlowerInputsWrapper>
+					<AddFlowerTextArea placeholder="Informacje o kwiatku" />
+					<img src={Pot} alt="" className={css`
+					position: absolute;
+					max-width: 100%;
+					top: -53px;
+					z-index: -1;
+					max-height: 357px;
+					left: 50%;
+					transform: translateX(-50%);
+					max-width: 320px;
+				`} />
 				</AddFlowerInputsWrapper>
 			</AddFlowerForm>
 		</AddFlowerWrapper>
--- src/components/modules/AddFlower/style.js
+++ src/components/modules/AddFlower/style.js
@@ -3,7 +3,11 @@ import styled from '@emotion/styled'
 export const AddFlowerForm = styled.form`
 	width: 100%;
 	height: 100%;
-	padding-top: 70px;
+
+    max-width: 360px;
+    margin: 0 auto;
+	padding: 120px 20px 0;
+
 `;
 
 export const AddFlowerFormImage = styled.img`
@@ -11,6 +15,8 @@ export const AddFlowerFormImage = styled.img`
     height: 150px;
     object-fit: cover;
 	border-radius: 100%;
+	position: relative;
+	
 `;
 
 export const AddFlowerInputsWrapper = styled.div`
@@ -18,9 +24,26 @@ export const AddFlowerInputsWrapper = styled.div`
 	flex-direction: column;
 	align-items: center;
 	position: relative;
+
+	&:first-of-type {
+		margin-bottom: 40px;
+
+
+		&:after {
+			content: '';
+			width: 20px;
+			height: 100px;
+			background: #7cb65b;
+			position: absolute;
+			bottom: -42px;
+			z-index: -1;
+		}
+
+	}
+
 `;
 
-export const AddFlowerInput = styled.input`
+export const AddFlowerInputFile = styled.input`
 	width: 150px;
     height: 150px;
     object-fit: cover;
@@ -48,14 +71,66 @@ export const AddFlowerHint = styled.button`
 	display: flex;
 	justify-content: center;
 	align-items: center;
+	line-height: 1;
 `;
 
 export const AddFlowerWrapper = styled.div`
 	opacity: 0;
 
 	transition: opacity 0.5s;
-	
+
+	overflow-y: auto;
+	height: 100%;
+
 	&.active {
 		opacity: 1;
 	}
+	
 `;
+
+
+export const AddFlowerInput = styled.input`
+	text-align: center;
+	width: 100%;
+	padding: 15px;
+	background: transparent;
+	border: none;
+	color: white;
+	outline: none;
+	line-height: 1;
+	font-size: 20px;
+	font-family: 'Roboto Condensed', sans-serif;
+	letter-spacing: 1px;
+
+	&::placeholder {
+		color: white;
+	}
+`;
+
+export const AddFlowerTextArea = styled.textarea`
+	text-align: center;
+	width: 100%;
+	padding: 15px;
+	background: transparent;
+	border: none;
+	outline: none;
+	font-family: 'Roboto Condensed', sans-serif;
+	letter-spacing: 1px;
+    max-width: 229px;
+	height: 220px;
+    color: #645143;
+    line-height: 1.2;
+    font-size: 24px;
+    margin-left: 7px;
+	resize: none;
+
+	&::placeholder {
+	    color: #645143;
+	}
+`;
+
+
+export const AddFlowerInputPattern = styled.div`
+	position: relative;
+`;
+
